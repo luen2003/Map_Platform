ec21aa577fca29b295d0916173a16221a3be4e7a
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-10-01 20:48:21 +0700

    change location features

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,41 +1,24 @@
 "use client";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useState } from "react";
 import SearchBox from "./SearchBox";
 
-// Define type for props of ChangeMapView
+// Định nghĩa kiểu cho props của ChangeMapView
 interface PositionProps {
-    position: [number, number]; // Latitude and Longitude
+    position: [number, number]; // Latitude và Longitude
 }
 
 const ChangeMapView: React.FC<PositionProps> = ({ position }) => {
     const map = useMap();
-    map.setView(position, 16); // Update the map's position to the coordinates
+    map.setView(position, 15); // Cập nhật vị trí của map theo tọa độ
     return null;
 };
 
 const VietnamMap = () => {
     const [position, setPosition] = useState<[number, number]>([
         21.0285, 105.8542,
-    ]); // Default coordinates of Vietnam
-
-    // Function to get user's current location
-    const locateUser = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const { latitude, longitude } = position.coords;
-                    setPosition([latitude, longitude]); // Update position to user's location
-                },
-                (error) => {
-                    console.error("Error getting user location:", error);
-                }
-            );
-        } else {
-            alert("Geolocation is not supported by this browser.");
-        }
-    };
+    ]); // Tọa độ mặc định của Việt Nam
 
     return (
         <div className="h-screen flex flex-col">
@@ -50,16 +33,7 @@ const VietnamMap = () => {
                 />
                 <ChangeMapView position={position} />
             </MapContainer>
-            {/* Container for the search box and locate button */}
-            <div className="fixed z-50 top-4 left-1/2 transform -translate-x-1/2 flex space-x-4">
-                <SearchBox setPosition={setPosition} /> {/* Search box for finding locations */}
-                <button
-                    onClick={locateUser} // Button for locating the user's position
-                    className="bg-green-500 text-white p-2 rounded-lg hover:bg-green-600 transition duration-300"
-                >
-                    Định vị vị trí
-                </button>
-            </div>
+            <SearchBox setPosition={setPosition} />
         </div>
     );
 };
